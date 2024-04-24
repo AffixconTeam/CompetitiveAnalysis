147e68cbb53b6c1a4fae2174a9d382d586eff72a
TuanMinhajAffixcon <tuan.seedin@affixcon.com.au>
2024-04-24 11:06:17 +0530

    code updated

--- pages/3_lookalike_model.py
+++ pages/3_lookalike_model.py
@@ -87,7 +87,7 @@ def PCA_plot(visitors_data_all,non_visitors_data):
     with col2:
         with st.expander('About', expanded=True):
             st.write("""
-                :orange[**The lookalike model uses behavioral data from home and work locations to identify similar customers. This data is visualized in a 2D scatter plot, with customers represented as points. 
+                -:orange[**The lookalike model uses behavioral data from home and work locations to identify similar customers. This data is visualized in a 2D scatter plot, with customers represented as points. 
                      By analyzing proximity in the plot, 
                      the model predicts potential customers with similar behaviors using nearest neighbor search.**]
                 -:orange[**columns used for this analysis are Home,Work location distances and Longitude,Latitude of all other Places visited**]
@@ -182,7 +182,7 @@ if options == 'Search by Radius':
 
     combined_data1 = pd.concat([visitors_data_all, non_visitors_data])
 
-    # st.write(combined_data1.iloc[nearest_neighbors_indices])
+    st.write(combined_data1)
 
 
 else:
